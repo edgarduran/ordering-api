48085a2448d4927194cb7eaf781f6c0ce5cb197c
Edgar Duran <edgar.eduran@gmail.com>
2016-05-25 12:09:40 -0600

    category and item models

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :items, only [:index, :show], defaults: {format: :json}
+  namespace :api do
+    namespace :v1 do
+      resources :items, only: [:index, :show], defaults: {format: :json}
+    end
+  end
 
 end
